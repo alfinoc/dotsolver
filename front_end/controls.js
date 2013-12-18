de6cc5f3a1df677869f2d252735fe6fef9719dec
alfinoc <chrisalfino@gmail.com>
2013-12-18 01:00:49 -0800

    pushing and popping going on

--- front_end/controls.js
+++ front_end/controls.js
@@ -22,14 +22,14 @@ function randInt(min, max) {
 
 function attachConnectorHandlers() {
 	$("circle").mousedown(function(event) {
-		makeFollowerLine(event.target);
-		pushLine(parseInt($(this).attr("id")));
+		lineManager.makeFollowerLine(event.target);
+		lineManager.pushLine(parseInt($(this).attr("id")));
 		bindAdjacentDotListeners(parseInt($(this).attr("id")), true)
 	});
 	$("body").mouseup(function() {
 		$("#boardwrap").unbind();
 		unbindMouseoverListeners();
-		clearLines();
+		lineManager.clearLines();
 	});
 }
 
@@ -40,100 +40,100 @@ function bindAdjacentDotListeners(id, first) {
 	for (var i = 1; i < 9; i += 2) {
 		var adjId = id - 1 - DIM + DIM * Math.floor(i / 3) + i % 3;
 		var adj = $("#" + adjId);
-		if (adj.attr("code") == code && !containsPair(id, adjId)) {
+		if (adj.attr("code") == code && !lineManager.containsPair(id, adjId))
 			adj.mouseenter(mouseoverCallback(adj, adjId));
-			adj.attr("stroke", "black");
-		}
 	}
+	// change this to: if mouseout in the direction of the connected piece, popLine
 	target.mouseenter(function() {
 		target.mouseleave(function() {
-			popLine(adjId);
+			lineManager.popLine(adjId);
 		});
 	});
 }
 
 function mouseoverCallback(adj, adjId) {
 	return function() {
-		pushLine(adjId);
+		lineManager.pushLine(adjId);
 		bindAdjacentDotListeners(adjId, false);
 	}
-}
-
-function makeFollowerLine(fromElem) {
-	var svg = $("svg");
-	var xOffset = svg.position().left;
-	var yOffset = svg.position().top;
-	var coord = getCenter($(fromElem));
-	var line = makeLine(coord[0], coord[1], coord[0], coord[1]);
-
-	svg.prepend(line);
-	$("#boardwrap").mousemove(function(event) {
-		line.setAttribute("x2", event.pageX - xOffset);
-		line.setAttribute("y2", event.pageY - yOffset);
-	});
-}
-
-function makeLine(x1, y1, x2, y2) {
-	var line = document.createElementNS('http://www.w3.org/2000/svg','line');
-	line.setAttribute("x1", x1);
-	line.setAttribute("y1", y1);
-	line.setAttribute("x2", x2);
-	line.setAttribute("y2", y2);
-	return line;
-}
-
-var idChain = [];
-var lineChain = [];
-function pushLine(id) {
-	var prev = idChain[idChain.length - 1];
-	if (!containsPair(id, prev)) {
-		idChain.push(id);
-		lineChain.push(connect(id, prev));
-		console.log("pushed! idChain: " + idChain);
-	}
-}
-
-function popLine(id) {
-	if (idChain.length > 1) {
-		idChain.pop(id);
-		bindAdjacentDotListeners(idChain[idChain.length - 1]);
-		console.log("popped! idChain: " + idChain);
-	}
-}
-
-function clearLines(id) {
-	idChain = [];
-	$("line").remove();
-}
-
-function containsPair(first, second) {
-	for (var i = 1; i < idChain.length; i++)
-		if ((idChain[i] == second && idChain[i - 1] == first) ||
-			(idChain[i] == first && idChain[i - 1] == second))
-			return true;
-	return false;
-}
-
-function connect(idStart, idEnd) {
-	if (idStart == undefined || idEnd == undefined) return;
-	var start = getCenter($("#" + idStart));
-	var end = getCenter($("#" + idEnd));
-	var line = makeLine(start[0], start[1], end[0], end[1])
-	$("svg").prepend(line);
-	return $(line);
-}
-
-function getCenter(elem) {
-	var svg = $("svg");
-	var xOffset = svg.position().left;
-	var yOffset = svg.position().top;
-	var radius = parseFloat(elem.attr("r")) / 100 * svg.width();
-	var cx = elem.position().left - xOffset + radius;
-	var cy = elem.position().top - yOffset + radius;
-	return [cx, cy];
 }
 
 function unbindMouseoverListeners() {
 	$("circle").unbind("mouseenter");
 	$("circle").unbind("mouseleave");
+}
+
+var lineManager = {
+	idChain: [],
+	lineChain: [],
+	pushLine: function(id) {
+		var prev = this.idChain[this.idChain.length - 1];
+		if (!this.containsPair(id, prev)) {
+			this.idChain.push(id);
+			this.lineChain.push(this.connect(id, prev));
+		}
+	},
+
+	popLine: function(id) {
+		if (this.idChain.length > 1) {
+			this.idChain.pop(id);
+			$(this.lineChain.pop()).remove();
+			bindAdjacentDotListeners(this.idChain[this.idChain.length - 1]);
+		}
+	},
+
+	makeFollowerLine: function(fromElem) {
+		var svg = $("svg");
+		var xOffset = svg.position().left;
+		var yOffset = svg.position().top;
+		var coord = this.getCenter($(fromElem));
+		var line = this.buildLine(coord[0], coord[1], coord[0], coord[1]);
+
+		svg.prepend(line);
+		$("#boardwrap").mousemove(function(event) {
+			line.setAttribute("x2", event.pageX - xOffset);
+			line.setAttribute("y2", event.pageY - yOffset);
+		});
+	},
+
+	connect: function(idStart, idEnd) {
+		if (idStart == undefined || idEnd == undefined) return;
+		var start = this.getCenter($("#" + idStart));
+		var end = this.getCenter($("#" + idEnd));
+		var line = this.buildLine(start[0], start[1], end[0], end[1])
+		$("svg").prepend(line);
+		return $(line);
+	},
+
+	buildLine: function(x1, y1, x2, y2) {
+		var line = document.createElementNS('http://www.w3.org/2000/svg','line');
+		line.setAttribute("x1", x1);
+		line.setAttribute("y1", y1);
+		line.setAttribute("x2", x2);
+		line.setAttribute("y2", y2);
+		return line;
+	},
+
+	clearLines: function(id) {
+		this.idChain = [];
+		$("line").remove();
+	},
+
+	containsPair: function(first, second) {
+		for (var i = 1; i < this.idChain.length; i++)
+			if ((this.idChain[i] == second && this.idChain[i - 1] == first) ||
+				(this.idChain[i] == first && this.idChain[i - 1] == second))
+				return true;
+		return false;
+	},
+
+	getCenter: function(elem) {
+		var svg = $("svg");
+		var xOffset = svg.position().left;
+		var yOffset = svg.position().top;
+		var radius = parseFloat(elem.attr("r")) / 100 * svg.width();
+		var cx = elem.position().left - xOffset + radius;
+		var cy = elem.position().top - yOffset + radius;
+		return [cx, cy];
+	}
 }
