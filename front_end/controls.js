dd03e7cc485e754cea8f4dfb5704e34e713f359e
alfinoc <chrisalfino@gmail.com>
2013-12-19 22:26:21 -0800

    playable, though no special move detection in the frontend yet

--- front_end/controls.js
+++ front_end/controls.js
@@ -1,11 +1,120 @@
 jQuery(function($) {
+   RAD = $("#0").attr("r");
+   RAD_PIXEL = parseFloat(RAD) / 100 * $("svg").width();
+   SEP = parseFloat($("#0").attr("cx")) / 100 * $("svg").width()
    assignColorTags();
    attachConnectorHandlers();
 });
 
-DIM = 6
-var colors = ["transparent", "blue", "orange", "red",
-                  "purple", "green", "yellow"];
+var DIM = 6; // board side dimension
+var RAD;  // circle radius (in percent of parent)
+var RAD_PIXEL; // circle radius (in pixels)
+var SEP;  // distance between circles
+
+var DECAY = 0.8;
+
+var score = 0;
+function makeMove(ids) {
+   score += ids.length;
+   ids.sort();
+   removeDuplicates(ids);
+   $("#scoreboard div").html(score);
+
+   shrinkDots(ids);
+   animateFalls(computeFalls(ids), ids);
+}
+
+function shrinkDots(ids) {
+   for (var i = 0; i < ids.length; i++) {
+      $("#" + ids[i]).animate({tabIndex: 0}, {
+         duration: 200,
+         step: function(now, fx) {
+            $(this).attr("r", (parseFloat($(fx.elem).attr("r")) * DECAY) + "%");
+         },
+         complete: function() {
+            $(this).attr("r", "0%");
+         }
+      });
+   }
+}
+
+function computeFalls(ids) {
+   var fallDist = [];
+   var emptyBelow = 0;
+   for (var i = DIM * DIM - 1; i >= 0; i--) {
+      if (i % DIM == DIM - 1)
+         emptyBelow = 0;
+      if (ids.indexOf(i) != -1)
+         emptyBelow++;
+      else
+         fallDist[i] = emptyBelow;
+   }
+   return fallDist;
+}
+
+function animateFalls(fallDist, ids) {
+   var svg = $("svg");
+   for (var i = 0; i < fallDist.length; i++) {
+      var dist = fallDist[i];
+      if (dist != undefined && dist != 0) {
+         var ghost = makeGhost($("#" + i), i + dist);
+         ghostAnimate(ghost, svg);
+      }
+   }
+   var newCounts = [0, 0, 0, 0, 0, 0];
+   for (var i = 0; i < ids.length; i++) {
+      newCounts[Math.floor(ids[i] / DIM)]++;
+   }
+   // load up fallDist[i] new dots to fall into empty spots
+   for (var i = 0; i < newCounts.length; i++) {
+      var top = $("#5");
+      var x = (i + 1) * SEP;
+      
+      var r = RAD;
+      for (var j = 0; j < newCounts[i]; j++) {
+         var y = (j - newCounts[i]) * SEP + "%";
+         var newDot = lineManager.makeCircle(x, y, r);
+         $(newDot).attr("code", randInt(1, 7));
+         var newDotGhost = makeGhost($(newDot), i * DIM + j);
+         ghostAnimate(newDotGhost, svg);
+      }
+   }
+}
+
+function makeGhost(source, destId) {
+   var ghost = $(lineManager.copyCircle(source));
+   ghost.addClass("ghost");
+   ghost.attr("dest", destId);
+   source.attr("r", "0%");
+   return ghost;
+}
+
+function ghostAnimate(ghost, svg) {
+   var startY = parseFloat(ghost.attr("cy"));
+   var endY = parseFloat($("#" + ghost.attr("dest")).attr("cy"))
+   svg.prepend(ghost);
+   ghost.animate({tabIndex: 0}, {
+      duration: 200,
+      easing: "linear",
+      progress: ghostFallCallback(ghost, startY, endY),
+      complete: ghostCompleteCallback(ghost)
+   });
+}
+
+function ghostFallCallback(ghost, startY, endY) {
+   return function(anim, prog, remaining) {
+      ghost.attr("cy", (startY + (endY - startY) * prog) + "%");
+   }
+}
+
+function ghostCompleteCallback(ghost) {
+   return function() {
+      var dest = $("#" + ghost.attr("dest"))
+      dest.attr("code", ghost.attr("code"));
+      dest.attr("r", ghost.attr("r"));
+      ghost.remove();
+   }
+}
 
 function assignColorTags() {
    var circles = $("circle");
@@ -15,7 +124,6 @@ function assignColorTags() {
    }
 }
 
-// returns a random integer between min and max exclusive
 function randInt(min, max) {
    return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -29,11 +137,15 @@ function attachConnectorHandlers() {
    $("body").mouseup(function() {
       $("#boardwrap").unbind();
       unbindMouseoverListeners();
+      if (lineManager.idChain.length > 1)
+         makeMove(lineManager.idChain);
       lineManager.clearLines();
    });
 }
 
 function bindAdjacentDotListeners(id, first) {
+   var svg = $("svg");
+   svg.unbind("mousemove");
    unbindMouseoverListeners();
    var target = $("#" + id);
    lineManager.setFollowerLine(target);
@@ -44,15 +156,9 @@ function bindAdjacentDotListeners(id, first) {
       if (adj.attr("code") == code && !lineManager.containsPair(id, adjId))
          adj.mouseenter(progressChainCallback(adj, adjId));
    }
-   // change this to: if mouseout in the direction of the connected piece, popLine
-
-   target.mouseenter(function() {
-      target.mouseleave(function() {
-         lineManager.popLine();
-      });
-   });
-
-   //target.mouseleave(retractChainCallback(target));
+   
+   if (lineManager.idChain.length > 1)
+      svg.mousemove(retractChainCallback(target, svg));
 }
 
 function progressChainCallback(adj, adjId) {
@@ -62,31 +168,34 @@ function progressChainCallback(adj, adjId) {
    }
 }
 
-// to do: this is hard...
-function retractChainCallback(target) {
-   var svg = $("svg");
+function retractChainCallback(target, svg) {
    var xOffset = svg.position().left;
    var yOffset = svg.position().top;
-   var center = lineManager.getCenter(target);
-   var prev = lineManager.idChain[lineManager.idChain - 2];
-
+   var prev = lineManager.idChain[lineManager.idChain.length - 2];
+   var center = lineManager.getCenter($("#" + prev));
 
    return function(event) {
       var x = event.pageX - xOffset;
       var y = event.pageY - yOffset;
-      // approximates a region of the dot that is closest to the previous
-      // dot in the chain
-      console.log(x < center[0]);
-      console.log(Math.abs(y - center[1]) < 50);
-      if (x < center[0] && Math.abs(y - center[1]) < 50)
+      var dist = Math.sqrt(Math.pow(x - center[0], 2) + Math.pow(y - center[1], 2));
+      if (dist < 3 * RAD_PIXEL) {
          lineManager.popLine();
+         svg.unbind(event);
+      }
    }
 }
-
 
 function unbindMouseoverListeners() {
    $("circle").unbind("mouseenter");
    $("circle").unbind("mouseleave");
+}
+
+function removeDuplicates(a) {
+   for (var i = 0; i < a.length - 1;)
+      if (a[i] == a[i + 1])
+         a.splice(i, 1);
+      else
+         i++;
 }
 
 var lineManager = {
@@ -142,6 +251,20 @@ var lineManager = {
       line.setAttribute("x2", x2);
       line.setAttribute("y2", y2);
       return line;
+   },
+
+   copyCircle: function(elem) {
+      return this.makeCircle(elem.attr("cx"), elem.attr("cy"),
+                             elem.attr("r"), elem.attr("code"));
+   },
+
+   makeCircle: function(cx, cy, radius, code) {
+      var circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
+      circle.setAttribute("cx", cx);
+      circle.setAttribute("cy", cy);
+      circle.setAttribute("r", radius);
+      circle.setAttribute("code", code);
+      return circle;
    },
 
    clearLines: function(id) {
